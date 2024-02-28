c99e2dd49cf6fe9635da38d1382cd336499b1733
Seba <sebastiansaavedr@gmail.com>
2024-02-28 18:34:45 -0300

    mejoras al calendario
    Falta crear un acordion para MisReservasView, crear un función en el backend para poder hacer una reserva semanal (el mismo día todas las semanas del mes), el modal de reserva ya fue modificado pero falta agregar un poco más de logica respecto a los meses seleccionables

--- frontend/src/components/CustomToolbar.jsx
+++ frontend/src/components/CustomToolbar.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types'
 import clsx from 'clsx'
 import { Navigate as navigate } from 'react-big-calendar';
+import { Dropdown } from 'react-bootstrap';
+import { setOfficeId, getAllOffices } from '../slices/oficinaSlice';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 function ViewNamesGroup({ views: viewNames, view, messages, onView }) {
@@ -32,6 +35,25 @@ export default function CustomToolbar({
   views,
 }) {
   const isMisReservasView = view === 'misReservas';
+  const dispatch = useDispatch();
+  const oficinas = useSelector(getAllOffices);
+  const [loading, setLoading] = useState(true);
+  const [selectedOffice, setOffice] = useState('');
+
+  useEffect(() => {
+    if (oficinas)
+    {
+      setOffice(oficinas[0].nombre);
+      dispatch(setOfficeId(oficinas[0].oficina_id));
+      setLoading(false);
+    }
+  }, [oficinas]);
+
+  const saveOffice_ReduxState = (office) => {
+    setOffice(office.nombre);
+    dispatch(setOfficeId(office.oficina_id));
+  }; 
+
   return (
     <div className="rbc-toolbar">
       <span className="rbc-btn-group">
@@ -44,6 +66,27 @@ export default function CustomToolbar({
       </span>
 
       <span className="rbc-toolbar-label">{label}</span>
+
+      <span className="rbc-btn-group">
+        <Dropdown>
+            <Dropdown.Toggle
+              variant=""
+              id="dropdown-basic"
+            >
+              {`Oficina ${loading ? 'Cargando...' : selectedOffice}`}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              {oficinas && oficinas.map(office => (
+                <Dropdown.Item
+                  key={office.id}
+                  onClick={() => saveOffice_ReduxState(office)}
+                >
+                  {office.nombre}
+                </Dropdown.Item>
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
+      </span>
 
       <span className={clsx('rbc-btn-group')}>
         <button
