0f75c9cd590b23ddcad70fc2e606427778eab014
Seba <sebastiansaavedr@gmail.com>
2024-03-13 18:07:30 -0300

    MisReservasView
    Refactorización del codigo MisReservasView, navbar ajustado

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,57 +1,22 @@
-// import { useState, useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { setOfficeId, getAllOffices } from '../slices/oficinaSlice';
-import { Container, Nav, Navbar/*, Dropdown*/ } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import cencoLogo from '../logo.svg';
 
 function NavigationBar() {
-  // const dispatch = useDispatch();
-  // const oficinas = useSelector(getAllOffices);
-  // const [loading, setLoading] = useState(true);
-  // const [selectedOffice, setOffice] = useState('');
-
-  // useEffect(() => {
-  //   if (oficinas)
-  //   {
-  //     setOffice(oficinas[0].nombre);
-  //     dispatch(setOfficeId(oficinas[0].oficina_id));
-  //     setLoading(false);
-  //   }
-  // }, [oficinas]);
-
-  // const saveOffice_ReduxState = (office) => {
-  //   setOffice(office.nombre);
-  //   dispatch(setOfficeId(office.oficina_id));
-  // }; 
   
   return (
     <>
       <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="home">Reserva de Salas</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Container fluid>
+          <Navbar.Brand href="home">
+            <img src={cencoLogo} alt="Centro de Reservas" />
+            Centro de Reservas
+          </Navbar.Brand>
+          {/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              {/* <Nav.Link href="/">Calendario</Nav.Link> */}
+              <Nav.Link href="/">Calendario</Nav.Link>
             </Nav>
-            {/* <Dropdown>
-              <Dropdown.Toggle
-                variant=""
-                id="dropdown-basic"
-              >
-                {`Oficina ${loading ? 'Cargando...' : selectedOffice}`}
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                {oficinas && oficinas.map(office => (
-                  <Dropdown.Item
-                    key={office.id}
-                    onClick={() => saveOffice_ReduxState(office)}
-                  >
-                    {office.nombre}
-                  </Dropdown.Item>
-                ))}
-              </Dropdown.Menu>
-          </Dropdown> */}
-          </Navbar.Collapse>
+          </Navbar.Collapse> */}
         </Container>
       </Navbar>
     </>
